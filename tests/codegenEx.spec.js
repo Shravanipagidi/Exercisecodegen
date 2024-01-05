f81a1f543f750c97c9556a88544fcc942b27783f
Shravanipagidi <shravanireddy7789@gmail.com>
2024-01-05 16:18:42 +0530

    Update codegenEx.spec.js

--- tests/codegenEx.spec.js
+++ tests/codegenEx.spec.js
@@ -1,12 +1,13 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
+  
   await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
   await page.getByPlaceholder('Username').click();
   await page.getByPlaceholder('Username').fill('Admin');
-  await page.getByPlaceholder('Password').click();
+  await page.getByPlaceholder('Username').press('Tab');
   await page.getByPlaceholder('Password').fill('admin123');
   await page.getByRole('button', { name: 'Login' }).click();
-  await page.getByText('Paul CollingsBrenna').click();
+  await page.locator('span').filter({ hasText: 'Paul CollingsBrenna' }).locator('i').click();
   await page.getByRole('menuitem', { name: 'Logout' }).click();
 });
